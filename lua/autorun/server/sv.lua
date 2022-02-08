7e1393c7d528b93caa49a7e3453829e2d18df507
ESSYREV <90062759+ESSYREV@users.noreply.github.com>
2022-02-08 20:42:52 +0300

    Create sv.lua

---
+++ lua/autorun/server/sv.lua
@@ -0,0 +1,28 @@
+local function addHook(name)
+  hook.Add(name,"esrv.AntiCrash.ByBigProps",function(ply,mdl,ent)
+  	if ent == nil then return end
+    if ent:GetModelScale() != 1 then
+      ent:Remove()
+    end
+  end)
+end
+
+addHook("PlayerSpawnedProp")
+addHook("PlayerSpawnedRagdoll")
+addHook("PlayerSpawnedEffect")
+  
+
+
+local function addHookAnother(name) -- Это такая же функция, как и прошлая, но в функции не указано mdl.
+  hook.Add(name,"esrv.AntiCrash.ByBigProps",function(ply,ent)
+  	if ent == nil then return end
+    if ent:GetModelScale() != 1 then
+      ent:Remove()
+    end
+  end)
+end
+
+addHook("PlayerSpawnedNPC")
+addHook("PlayerSpawnedSENT")
+addHook("PlayerSpawnedSWEP")
+addHook("PlayerSpawnedVehicle")
